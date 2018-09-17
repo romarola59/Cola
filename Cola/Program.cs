5de3e02f21e04dc79752b06abe3420d08bf67ebd
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-17 20:05:14 -0300

    Cola

--- Cola/Program.cs
+++ Cola/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections;
 
 namespace Cola
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
+
         {
-            Console.WriteLine("Hello World!");
+            String palabra;  // se declara la variable string
+            Queue miCola = new Queue();  // Genera la variable miCola y a la inicia como nueva Cola.
+            miCola.Enqueue("hola");  // ingreso la primera palabra
+            miCola.Enqueue("Soy");  //ingreso la segunda palabra
+            miCola.Enqueue("yo");  // ingreso la tercera palabra
+            for(byte i = 0; i<3;i++)   // creamos un for para que itere cada una de las palabras hasta el final
+            {
+                palabra = (String) miCola.Dequeue(); // le asignamos el valor actual de la cola y el comando Dequeue para que haga la salida(saca la palabra).
+                Console.WriteLine(palabra); // se manda a que salga  en la consola palabra.
+            }
+            Console.ReadKey();
+
         }
     }
 }
